707c35981b6ba0279289e1b19eb84bf670ceeac9
James Trowbridge <jamestrowbridge@james-trowbridges-macbook-pro.local>
2011-10-10 18:10:05 -0400

    yay basic blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111007010725) do
+ActiveRecord::Schema.define(:version => 20111010215750) do
 
   create_table "jimbos", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "body"
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
